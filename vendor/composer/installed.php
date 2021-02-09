814bd9aa817f09cb8e327e4c4ef494949a6968d5
Richie Tan <richiegentan14@gmail.com>
2021-02-09 17:32:00 +0800

    testcommit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'de12f7f9fe378e20b7c6c4101ce83580a3b48164',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'de12f7f9fe378e20b7c6c4101ce83580a3b48164',
+    ),
+  ),
+);
